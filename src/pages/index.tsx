b2338879d73bb8bdd6988dc47a9fcf14f9cb8bf5
Akaasyah Nurfath <54873221+akasz56@users.noreply.github.com>
2023-04-18 03:02:15 +0700

    feat: index restructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,36 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div id='Hero' className="relative isolate overflow-hidden pt-14 h-screen">
-        <Image src="/hero.png" alt="Hero Background" width={1920} height={988} className="absolute inset-0 -z-10 h-full w-full object-cover" />
-
-        <div className="absolute bottom-44 right-14 w-1/2">
-          <h1 className="text-5xl font-normal leading-normal tracking-wider uppercase">
-            Lorem ipsum dolor sit amet,
-            consectetuer adipiscing elit,
-            sed diam nonummy nibh Euismo.
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-            fugiat veniam occaecat fugiat aliqua.
-          </p>
+      <div id="Hero" className="relative isolate overflow-hidden h-[85vh]">
+        <Image
+          src="/img/hero.png"
+          alt="Hero Background"
+          width={1920}
+          height={988}
+          className="absolute inset-0 -z-10 h-full w-full object-cover"
+        />
+        <div className="relative mx-auto h-full w-4/5 max-w-screen-2xl">
+          <div className="absolute bottom-0 right-0 mx-6 sm:ml-0 sm:mr-8 mb-16 sm:mb-24 lg:mb-28 w-9/12 xl:w-7/12">
+            <h1 className="uppercase tracking-wider text-2xl md:text-4xl lg:text-5xl sm:leading-normal md:leading-normal lg:leading-normal text-white">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh Euismo.
+            </h1>
+            <p className="mt-6 uppercase tracking-wider text-gray-300 text-xs sm:text-base lg:text-lg">
+              PT. Asya Kreasi Dahayu
+            </p>
+          </div>
         </div>
       </div>
+
+      <div id="About"></div>
+
+      <div id="Service"></div>
+
+      <div id="Project"></div>
+
+      <div id="Contact"></div>
     </>
-  )
+  );
 }
