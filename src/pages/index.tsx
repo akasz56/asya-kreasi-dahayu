586edd6ba7c8f01fe1a86ee988a2f7777665356f
Akaasyah Nurfath <54873221+akasz56@users.noreply.github.com>
2023-04-20 23:19:37 +0700

    test: sticky about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div id="Hero" className="relative isolate overflow-hidden h-[85vh]">
+      <div id="Hero">
         <Image
           src="/img/hero.png"
           alt="Hero Background"
           width={1920}
           height={988}
-          className="absolute inset-0 -z-10 h-full w-full object-cover"
+          className="object-cover"
         />
         <div className="relative mx-auto h-full w-4/5 max-w-screen-2xl">
           <div className="absolute bottom-0 right-0 mx-6 sm:ml-0 sm:mr-8 mb-16 sm:mb-24 lg:mb-28 w-9/12 xl:w-7/12">
@@ -24,23 +24,23 @@ export default function Home() {
         </div>
       </div>
 
-      <div
-        id="About"
-        className="border-b border-neutral-400 md:flex md:h-[60vh]"
-      >
-        <div className="md:w-2/5">
-          <Image
-            src="/img/about.jpg"
-            alt="About Image"
-            width={1920}
-            height={1080}
-            className="h-full object-cover"
-          />
+      <div id="About" className="border-b border-neutral-400 md:flex">
+        <div className="relative md:w-2/5">
+          <div className="flex h-screen md:sticky md:top-0">
+            <Image
+              src="/img/about.jpg"
+              alt="About Image"
+              width={1920}
+              height={1080}
+              className="h-full object-cover"
+            />
+          </div>
         </div>
-        <div className="flex-1 md:overflow-scroll px-6 pt-9">
-          <h1 className="uppercase text-4xl mb-14">About Us</h1>
+        <div className="flex-1 px-6 pt-9">
+          <h1 className="uppercase text-4xl mb-96">About Us</h1>
           <div className="flex flex-col items-end">
-            <div className="mb-14 w-3/4 max-w-lg">
+            <div className="mb-96 w-3/4 max-w-lg">
+              {/* <div className="mb-14 w-3/4 max-w-lg"> */}
               <p className="text-sm leading-normal tracking-wider">
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
                 diam nonummy nibh euismod tincidunt ut laoreet dolore magna
@@ -54,7 +54,8 @@ export default function Home() {
                 PT. Asya Kreasi Dahayu
               </p>
             </div>
-            <div className="mb-14 w-3/4 max-w-lg">
+            <div className="mb-96 w-3/4 max-w-lg">
+              {/* <div className="mb-14 w-3/4 max-w-lg"> */}
               <h2 className="mb-4 uppercase text-2xl tracking-wide">Visi</h2>
               <p className="text-sm leading-normal tracking-wider">
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
@@ -66,7 +67,8 @@ export default function Home() {
                 illum dolore eu feugiat nulla facilisis at vero eros et
               </p>
             </div>
-            <div className="mb-14 w-3/4 max-w-lg">
+            <div className="mb-96 w-3/4 max-w-lg">
+              {/* <div className="mb-14 w-3/4 max-w-lg"> */}
               <h2 className="mb-4 uppercase text-2xl tracking-wide">Misi</h2>
               <p className="text-sm leading-normal tracking-wider">
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
