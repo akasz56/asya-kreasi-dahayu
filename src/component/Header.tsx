81e93ed244ab2200b90a5b56f88a9471c184cdc7
Akaasyah Nurfath <54873221+akasz56@users.noreply.github.com>
2023-06-10 17:46:37 +0700

    changed navbar color

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -14,15 +14,15 @@ export default function Header() {
   ];
 
   return (
-    <header className="fixed w-full top-0 z-40 bg-neutral-900">
+    <header className="fixed w-full top-0 z-40 bg-asya">
       <nav
-        className="mx-auto px-6 lg:px-4 max-w-screen-2xl h-24 flex items-center justify-between"
+        className="my-8 mx-6 lg:mx-16 flex items-center justify-between"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
           <Link
             href={"/"}
-            className="text-xl font-normal leading-[1.5em] tracking-wider text-white"
+            className="text-xl leading-normal tracking-wider text-asya-dark"
           >
             ASYA KREASI DAHAYU
           </Link>
@@ -30,20 +30,20 @@ export default function Header() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-asya-dark"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <div className="hidden lg:flex">
           <ul className="flex space-x-16">
             {pages.map((item, key) => (
               <li key={key}>
                 <Link
                   href={item.href}
-                  className="text-xl font-normal leading-[1.5em] tracking-wider text-white"
+                  className="text-xl leading-normal tracking-wider text-asya-dark"
                 >
                   {item.title}
                 </Link>
@@ -76,31 +76,35 @@ export default function Header() {
                   leaveTo="translate-x-full"
                 >
                   <Dialog.Panel className="pointer-events-auto w-screen max-w-sm">
-                    <div className="flex h-full flex-col overflow-y-scroll bg-neutral-300 py-6 shadow-xl">
-                      <div className="px-4 sm:px-6">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-asya-dark py-6 shadow-xl">
+                      <div className="mx-6">
                         <div className="flex items-start justify-end">
-                          <div className="ml-3 flex h-7 items-center">
-                            <button
-                              type="button"
-                              className="rounded-md bg-neutral-300 text-black focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                              onClick={() => setMobileMenuOpen(false)}
-                            >
-                              <span className="sr-only">Close panel</span>
-                              <XMarkIcon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                            </button>
-                          </div>
+                          <button
+                            type="button"
+                            className="rounded-md bg-asya-dark text-asya"
+                            onClick={() => setMobileMenuOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon
+                              className="h-12 w-12"
+                              aria-hidden="true"
+                            />
+                          </button>
                         </div>
                       </div>
-                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                      <div className="relative flex-1 m-6">
                         <div className="flex flex-col">
+                          <Link
+                            href={"/"}
+                            className="uppercase text-asya text-4xl font-medium leading-normal tracking-wider mb-6"
+                          >
+                            Home
+                          </Link>
                           {pages.map((item, key) => (
                             <Link
                               key={key}
                               href={item.href}
-                              className="text-black text-3xl font-medium leading-[3rem] tracking-wider mb-3"
+                              className="uppercase text-asya text-4xl font-medium leading-normal tracking-wider mb-6"
                             >
                               {item.title}
                             </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        asya: {
+          DEFAULT: "#B6CD65",
+          dark: "#4A5842",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
