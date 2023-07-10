350773e122939db21be3b97df8a2391b80fb98ad
Akaasyah Nurfath <54873221+akasz56@users.noreply.github.com>
2023-07-10 21:42:44 +0700

    contact rework

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
-    return (
-        <div>Contact</div>
-    )
+  return (
+    <div id="contact" className="px-52 py-32 bg-white">
+      <h1 className="text-asya-dark text-5xl font-medium leading-normal tracking-widest">
+        Lorem Ipsum Dolor Sit, <br /> Contact Us Through
+      </h1>
+      <table className="w-1/2 mt-24 ml-auto text-asya-dark text-2xl">
+        <tr>
+          <td className="py-2 align-top">Phone</td>
+          <td className="py-2 px-8 align-top"> : </td>
+          <td className="py-2 align-top">+6212345678</td>
+        </tr>
+        <tr>
+          <td className="py-2 align-top">Email</td>
+          <td className="py-2 px-8 align-top"> : </td>
+          <td className="py-2 align-top">Info@asya.com</td>
+        </tr>
+        <tr>
+          <td className="py-2 align-top">Address</td>
+          <td className="py-2 px-8 align-top"> : </td>
+          <td className="py-2 align-top">
+            Jl. Ring Road Bubulak No. A-4, Kelurahan Bubulak, Kec. Bogor Barat,
+            Kota Bogor, Provinsi Jawa Barat, 16115
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
