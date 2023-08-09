8154400c2b62e6dc2d9a6273dc0fccd7c0530f4c
akasz56 <akaasyahn@gmail.com>
2023-08-09 13:12:41 +0700

    feat: web adjusted about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,47 +1,30 @@
 import ScrollArrow from "@/component/ScrollArrow";
-import { motion } from "framer-motion";
 import React from "react";
 
 export default function About() {
-  const section = {
-    visible: { transition: { staggerChildren: 0.5 } },
-  };
-
-  const item = {
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.5 },
-    },
-    hidden: { opacity: 0, y: 25 },
-  };
-
   return (
     <>
-      <motion.div
+      <section
         id="hero"
-        initial="hidden"
-        variants={section}
-        whileInView="visible"
-        className="relative h-screen bg-hero-about bg-cover bg-center flex flex-col gap-8 justify-center items-center"
+        className="relative h-screen bg-hero-about bg-cover bg-center flex flex-col items-center justify-center gap-6"
       >
-        <motion.p
-          variants={item}
-          className="text-asya-light text-center text-2xl md:text-4xl lg:text-5xl font-medium leading-normal md:leading-normal lg:leading-normal tracking-widest"
-        >
+        <p className="text-center text-asya-light text-5xl font-medium tracking-widest leading-normal">
           Get To Know Asya Kreasi Dahayu, <br /> Lorem Ipsum Dolor Sit Amet
           Consect
-        </motion.p>
-        <ScrollArrow scrollTo="#visi" />
-      </motion.div>
+        </p>
+        <ScrollArrow scrollTo="#visi" className="absolute bottom-0 mb-36" />
+      </section>
 
-      <div id="visi">
-        <div className="asya-container pt-36">
-          <div className="flex flex-wrap justify-between pb-36 border-b-2 border-asya-dark">
-            <h1 className="basis-5/12 uppercase text-asya-dark text-4xl font-medium tracking-widest">
+      <section id="visi" className="container mx-auto">
+        <div className="flex flex-wrap justify-between py-24 border-b-2 border-asya-dark">
+          <div className="basis-5/12">
+            <h2 className="uppercase text-asya-dark text-3xl font-medium tracking-widest">
               Visi Asya Kreasi Dahayu
-            </h1>
-            <p className="basis-5/12 text-asya-dark tracking-widest leading-relaxed">
+            </h2>
+            <div className="mt-16 w-4/5 aspect-square bg-asya-dark"></div>
+          </div>
+          <div className="basis-5/12">
+            <p className="text-asya-dark font-light tracking-widest leading-relaxed">
               Visi utama kami adalah memberikan pelayanan paling unggul dan
               perhatian yang tinggi kepada para kreator dengan ekosistem kerja
               yang inovatif dan produktif. Hal ini dilandasi dari cita-cita kami
@@ -49,46 +32,45 @@ export default function About() {
               di Indonesia dalam hal pelayanan dan kinerja untuk klien dan mitra
               kerja agar dapat menjadi pilar perekonomian negara.
             </p>
-            <div className="basis-5/12 aspect-square bg-asya-light"></div>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div id="misi">
-        <div className="asya-container pt-36">
-          <div className="flex flex-wrap justify-between pb-36 border-b-2 border-asya-dark">
-            <h1 className="basis-5/12 uppercase text-asya-dark text-4xl font-medium tracking-widest">
+      <section id="misi" className="container mx-auto">
+        <div className="flex flex-wrap justify-between py-24 border-b-2 border-asya-dark">
+          <div className="basis-5/12">
+            <h2 className="uppercase text-asya-dark text-3xl font-medium tracking-widest">
               Misi Asya Kreasi Dahayu
-            </h1>
-            <div className="basis-5/12 text-asya-dark tracking-widest leading-relaxed">
-              <p className="mb-4">
-                Dalam mewujudkan visi kami, kesejahteraan tim serta keseimbangan
-                ekosistem kerja adalah yang utama. Kami menciptakan lingkungan
-                kerja yang aman dan nyaman serta memberikan kesempatan
-                berkembang dengan membina dan meningkatkan semangat yang tinggi
-                kepada tim kami agar setiap level dari tim dapat bekerja sama
-                untuk mewujudkan pelayanan terbaik dan berkualitas sehingga
-                tercipta hubungan kerja sama yang kuat dengan pelanggan dan
-                mitra kerja.
-              </p>
-              <p>
-                Selain itu, kami menerapkan manajemen proyek yang terintegrasi
-                dengan wilayah operasi di Indonesia agar dapat memberikan
-                kepuasan untuk pelanggan, anggota, dan perusahaan serta
-                meningkatkan nilai yang tinggi bagi investor atau pemegang
-                saham.
-              </p>
-            </div>
-            <div className="basis-5/12 aspect-square bg-asya-light"></div>
+            </h2>
+            <div className="mt-16 w-4/5 aspect-square bg-asya-dark"></div>
+          </div>
+          <div className="basis-5/12">
+            <p className="mb-4 text-asya-dark font-light tracking-widest leading-relaxed">
+              Dalam mewujudkan visi kami, kesejahteraan tim serta keseimbangan
+              ekosistem kerja adalah yang utama. Kami menciptakan lingkungan
+              kerja yang aman dan nyaman serta memberikan kesempatan berkembang
+              dengan membina dan meningkatkan semangat yang tinggi kepada tim
+              kami agar setiap level dari tim dapat bekerja sama untuk
+              mewujudkan pelayanan terbaik dan berkualitas sehingga tercipta
+              hubungan kerja sama yang kuat dengan pelanggan dan mitra kerja.
+            </p>
+            <p className="text-asya-dark font-light tracking-widest leading-relaxed">
+              Selain itu, kami menerapkan manajemen proyek yang terintegrasi
+              dengan wilayah operasi di Indonesia agar dapat memberikan kepuasan
+              untuk pelanggan, anggota, dan perusahaan serta meningkatkan nilai
+              yang tinggi bagi investor atau pemegang saham.
+            </p>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div id="team">
-        <div className="asya-container">
-          <div className="py-36 asya-section-title">
-            <h1 className="text-asya-dark">Our Team</h1>
-            <p className="text-asya-dark">
+      <section id="team">
+        <div className="container mx-auto">
+          <div className="flex items-center my-20">
+            <h2 className="flex-1 uppercase text-asya-dark text-3xl font-medium tracking-widest">
+              Our Team
+            </h2>
+            <p className="w-[70%] text-asya-dark font-light tracking-widest leading-relaxed">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
               nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
               erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
@@ -98,7 +80,7 @@ export default function About() {
               nostrud exercita.
             </p>
           </div>
-          <div className="pb-36 grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-4 gap-4 mb-36">
             {[...Array(16)].map((item, key) => (
               <a
                 href="#"
@@ -118,7 +100,7 @@ export default function About() {
             ))}
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
