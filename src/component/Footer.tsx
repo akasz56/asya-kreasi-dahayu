467db1555e7b1d9956cabb4d28b37ffced325c4a
akasz56 <akaasyahn@gmail.com>
2023-08-13 17:54:22 +0700

    fix: homepage alignment

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,33 +3,33 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-asya-dark">
-      <div className="container flex justify-between items-center mx-auto py-16">
-        <h2 className="uppercase text-asya-light text-3xl font-medium tracking-widest">
+      <div className="container mx-auto py-16 flex justify-between items-center">
+        <h2 className="uppercase text-white text-3xl font-medium tracking-widest">
           Asya Kreasi Dahayu
         </h2>
         <div className="space-y-12">
-          <p className="text-asya-light text-md font-medium tracking-widest">
+          <p className="text-white text-md font-medium tracking-widest">
             We'd love to talk to you about our approach <br /> and how we might
             be able to help your brand.
           </p>
           <div className="relative">
             <label
               htmlFor="email"
-              className="text-asya-light text-md font-medium tracking-widest"
+              className="text-white text-md font-medium tracking-widest"
             >
               Email Address
             </label>
             <input
               type="text"
               id="email"
-              className="block w-full border-b-2 border-asya-light bg-transparent py-1.5 mt-2 text-asya-light text-xl font-medium tracking-widest focus:outline-none"
+              className="block w-full border-b-2 border-white bg-transparent py-1.5 mt-2 text-white text-xl font-medium tracking-widest focus:outline-none"
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="absolute top-0 right-0 cursor-pointer stroke-asya-light w-10"
+              className="absolute top-0 right-0 cursor-pointer stroke-white w-10"
             >
               <path
                 strokeLinecap="round"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,25 +16,25 @@ export default function Home() {
         id="hero"
         className="relative h-screen bg-hero-landing bg-cover bg-center flex flex-col items-center justify-center gap-6"
       >
-        <p className="text-center text-asya-light text-5xl font-medium tracking-widest leading-normal">
+        <p className="text-center text-white text-5xl font-medium tracking-widest leading-normal">
           Menghidupkan ambisi dalam berkreasi. <br /> Bersama Asya nyalakan
           kembali asa.
         </p>
         <a
           href="#"
-          className="px-6 py-4 rounded-md bg-asya-light uppercase text-asya-dark font-medium tracking-widest"
+          className="px-6 py-2 rounded-md bg-asya-dark uppercase text-white font-medium tracking-widest"
         >
           Get started
         </a>
-        <ScrollArrow scrollTo="#about" className="absolute bottom-0 mb-36" />
+        <ScrollArrow scrollTo="#about" className="absolute bottom-0 mb-[5vw]" />
       </section>
 
       <section id="about" className="bg-asya-dark">
-        <div className="container flex items-center mx-auto py-20">
-          <h2 className="flex-1 uppercase text-asya-light text-3xl font-medium tracking-widest">
+        <div className="flex container mx-auto py-20">
+          <h2 className="flex-1 self-center uppercase text-white text-5xl font-medium tracking-widest">
             About Us
           </h2>
-          <p className="w-[70%] text-asya-light font-light tracking-widest leading-relaxed">
+          <p className="w-2/3 text-white font-light tracking-widest leading-relaxed">
             Asya Kreasi Dahayu merupakan perusahaan yang bergerak di bidang
             industri kreatif dan pengembangan sumber daya manusia. Sejak
             September 2022, kami memberikan pelayanan untuk memudahkan anda
@@ -48,11 +48,11 @@ export default function Home() {
 
       <section id="service">
         <div className="container mx-auto">
-          <div className="flex items-center py-20">
-            <h2 className="flex-1 uppercase text-asya-dark text-3xl font-medium tracking-widest">
+          <div className="flex py-20">
+            <h2 className="flex-1 self-center uppercase text-asya-dark text-5xl font-medium tracking-widest">
               Our Services
             </h2>
-            <p className="w-[70%] text-asya-dark font-light tracking-widest leading-relaxed">
+            <p className="w-2/3 text-asya-dark font-light tracking-widest leading-relaxed">
               Sejak berdiri, Asya Kreasi Dahayu telah membantu banyak kreator di
               bidang industri kreatif dari seluruh Indonesia. Fokus kami adalah
               menyediakan solusi dengan menjadi media penghubung untuk
@@ -63,40 +63,20 @@ export default function Home() {
               untuk berkembang dan ruang untuk saling berkolaborasi.
             </p>
           </div>
-          <div className="max-w-screen-xl mx-auto flex flex-wrap justify-between">
+          <div className="mb-28 grid grid-cols-2 gap-16">
             {services.map((item, key) => (
-              <Link key={key} href={"#"} className="w-1/2 px-8 mb-16">
-                <div className="rounded-3xl aspect-square bg-asya-light"></div>
-                <h2 className="mt-8 uppercase text-asya-dark text-3xl font-medium tracking-widest">
+              <Link key={key} href={"#"}>
+                <div className="rounded-3xl aspect-square bg-asya-dark"></div>
+                <h2 className="mt-20 uppercase text-asya-dark text-3xl font-medium tracking-widest">
                   {item.title}
                 </h2>
-                <p className="mt-4 text-asya-dark text-sm tracking-widest">
+                <p className="mt-20 text-asya-dark text-sm tracking-widest">
                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
                   diam nonummy nibh euismod tincidunt ut laoreet dolore magna
                   aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
                   nostrud exercita.
                 </p>
               </Link>
-              // <div  className="flex flex-col justify-between gap-8">
-              //   <div className="rounded-3xl aspect-square bg-asya-light"></div>
-              //   <div className="flex gap-8">
-              //     <h2 className="flex-1 uppercase text-asya-dark text-3xl font-medium tracking-widest">
-              //       {item.title}
-              //     </h2>
-              //     {/* <Link
-              //       href={item.href}
-              //       className="self-center rounded-md bg-asya-light px-8 py-2 uppercase text-sm tracking-widest text-asya-dark shadow-sm hover:opacity-50"
-              //     >
-              //       Read More
-              //     </Link> */}
-              //   </div>
-              //   <p className=" text-asya-dark text-sm tracking-widest">
-              //     Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-              //     diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-              //     aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-              //     nostrud exercita.
-              //   </p>
-              // </div>
             ))}
           </div>
         </div>
