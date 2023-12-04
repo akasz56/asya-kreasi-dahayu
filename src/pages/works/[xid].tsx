c65354e9b52aa00adfb1434aa844f18d6f7325ec
secondlast21 <secondlast2102@gmail.com>
2023-12-04 20:26:26 +0700

    refactor(works): handle routing page workdetail

--- src/pages/works/[xid].tsx
+++ src/pages/works/[xid].tsx
@@ -7,20 +7,25 @@ import { worksData, Work } from './constant'
 
 const WorkDetail: NextPage = () => {
   const router = useRouter()
-  if (!router.query.xid) router.push('/works')
-  if (typeof router.query.xid !== 'string') router.push('/works')
-  const worksDetail: Work = worksData.filter((item) => item.xid === router.query.xid)[0]
-  if (!worksDetail) router.push('/works')
+  const [workDetail, setWorkDetail] = useState<Work>()
+  
+  useEffect(() => {
+    if (!router.query.xid) router.push('/works')
+    if (typeof router.query.xid !== 'string') router.push('/works')
+
+    setWorkDetail(worksData.filter((item) => item.xid === router.query.xid)[0])
+    if (!worksData.filter((item) => item.xid === router.query.xid)[0]) router.push('/works')
+  }, [router.query.xid])
 
   return (
     <CustomHead
-      title={worksDetail?.title ?? 'Detail Work'}
-      description={worksDetail?.description}
+      title={workDetail?.title ?? 'Detail Work'}
+      description={workDetail?.description}
     >
       <div className='asya-container'>
         <div className='flex'>
           <div className='w-1/2 space-y-12'>
-            {worksDetail?.asset.map((item, key) => (
+            {workDetail?.asset.map((item, key) => (
               <Image
                 key={key}
                 src={'/images/hero.jpg'}
@@ -34,10 +39,10 @@ const WorkDetail: NextPage = () => {
           <div className='w-1/2'>
             <div className='h-screen sticky top-0 ml-36 py-32 flex flex-col'>
               <h3 className='flex-1 uppercase text-asya-dark text-4xl font-bold tracking-widest leading-normal'>
-                {worksDetail?.title}
-                <span className='block text-xl font-normal tracking-widest'>{worksDetail?.subtitle}</span>
+                {workDetail?.title}
+                <span className='block text-xl font-normal tracking-widest'>{workDetail?.subtitle}</span>
               </h3>
-              <p className='text-asya-dark text-base tracking-widest leading-normal'>{worksDetail?.description}</p>
+              <p className='text-asya-dark text-base tracking-widest leading-normal'>{workDetail?.description}</p>
             </div>
           </div>
         </div>
