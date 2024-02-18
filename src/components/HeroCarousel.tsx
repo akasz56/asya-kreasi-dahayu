8c40547aa50f2f02fbc04579d78df9fe3e127c10
akasz56 <akaasyahn@gmail.com>
2024-02-18 12:35:58 +0700

    fix: slider

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -2,7 +2,6 @@ import { useRef, useState, Fragment } from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { Carousel } from 'flowbite-react'
-import ScrollArrow from '@/components/ScrollArrow'
 import { Dialog, Transition } from '@headlessui/react'
 
 export default function HeroCarousel() {
@@ -22,37 +21,10 @@ export default function HeroCarousel() {
     hidden: { opacity: 0, y: 25 },
   }
 
-  const playVideo = function () {
-    if (videoRef.current) {
-      videoRef.current.play()
-      videoRef.current.classList.remove('test')
-    } else console.error('videoRef not found!')
-  }
-
-  const pauseVideo = function () {
-    if (videoRef.current) {
-      videoRef.current.pause()
-      videoRef.current.classList.add('test')
-    } else console.error('videoRef not found!')
-  }
-
-  const autoplayVideo = function (index: Number) {
-    if (index == 1) playVideo()
-    else pauseVideo()
-  }
-
-  const onclickVideo = function () {
-    if (videoRef.current) {
-      if (videoRef.current.paused) playVideo()
-      else pauseVideo()
-    }
-  }
-
   return (
     <Carousel
       pauseOnHover
       slideInterval={5000}
-      onSlideChange={autoplayVideo}
       draggable={false}
       indicators={false}
     >
@@ -60,46 +32,49 @@ export default function HeroCarousel() {
         initial='hidden'
         variants={variants}
         whileInView='visible'
-        className="relative h-screen bg-[url('/images/hero1.jpg')] bg-cover bg-center flex flex-col items-center justify-center gap-6"
+        className="relative flex h-screen flex-col items-center justify-center gap-6 bg-[url('/images/hero1.jpg')] bg-cover bg-center"
       >
         <motion.p
           variants={firstChildVariant}
-          className='text-center text-white text-5xl font-medium tracking-widest leading-normal'
+          className='text-center text-5xl font-medium leading-normal tracking-widest text-white'
         >
           Menghidupkan ambisi dalam berkreasi. <br /> Bersama Asya nyalakan kembali asa.
         </motion.p>
         <motion.a
           href='/our-services'
           variants={firstChildVariant}
-          className='px-6 py-2 rounded-md bg-asya-dark uppercase text-white font-medium tracking-widest'
+          className='rounded-md bg-asya-dark px-6 py-2 font-medium uppercase tracking-widest text-white'
         >
           Get started
         </motion.a>
-        <ScrollArrow
-          scrollTo='#about'
-          className='absolute bottom-0 mb-[5vw]'
-        />
       </motion.div>
 
-      <div>
-        {/* 1. The button */}
+      <div className='relative h-screen'>
         <button
-          className='relative flex justify-center items-center focus:outline-none focus-visible:ring focus-visible:ring-indigo-300 rounded-3xl group'
+          className='h-full w-full'
           onClick={() => {
             setModalOpen(true)
           }}
           aria-label='Watch the video'
         >
+          <div className='absolute flex h-full w-full items-center justify-center bg-neutral-700 opacity-50'>
+            <svg
+              className='h-64 w-64'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 448 512'
+            >
+              <path d='M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z' />
+            </svg>
+          </div>
           <video
-            loop
-            controls
             autoPlay
-            className='h-screen w-screen object-cover object-center cursor-pointer'
+            loop
+            muted
+            className='h-full w-full cursor-pointer object-cover object-center'
           >
             <source
               src='./videos/hero_compro.mp4'
               type='video/mp4'
-              className='w-screen'
             />
             Your browser does not support the video tag.
           </video>
@@ -114,7 +89,6 @@ export default function HeroCarousel() {
             initialFocus={videoRef}
             onClose={() => setModalOpen(false)}
           >
-            {/* 2. The backdrop layer */}
             <Transition.Child
               className='fixed inset-0 z-[99999] bg-black bg-opacity-50 transition-opacity'
               enter='transition ease-out duration-200'
@@ -126,7 +100,6 @@ export default function HeroCarousel() {
               aria-hidden='true'
             />
 
-            {/* 3. The modal video */}
             <Transition.Child
               className='fixed inset-0 z-[99999] flex p-6'
               enter='transition ease-out duration-300'
@@ -136,12 +109,11 @@ export default function HeroCarousel() {
               leaveFrom='opacity-100 scale-100'
               leaveTo='opacity-0 scale-75'
             >
-              <div className='max-w-5xl mx-auto h-full flex items-center'>
-                <Dialog.Panel className='w-full max-h-full rounded-3xl shadow-2xl aspect-video bg-black overflow-hidden'>
+              <div className='mx-auto flex h-full max-w-6xl items-center'>
+                <Dialog.Panel className='aspect-video max-h-full w-full overflow-hidden rounded-3xl bg-black shadow-2xl'>
                   <video
                     width='1920'
                     height='1080'
-                    loop
                     controls
                     ref={videoRef}
                   >
@@ -161,25 +133,25 @@ export default function HeroCarousel() {
       <Image
         src={'/images/hero2.jpg'}
         alt='Hero Picture 2'
-        width={1920}
-        height={1440}
-        className='h-screen object-cover object-center'
+        width={7952}
+        height={5304}
+        className='relative h-screen object-cover object-center'
       />
 
       <Image
         src={'/images/hero3.jpg'}
         alt='Hero Picture 3'
-        width={1920}
-        height={1440}
-        className='h-screen object-cover object-center'
+        width={7952}
+        height={5304}
+        className='relative h-screen object-cover object-center'
       />
 
       <Image
         src={'/images/hero4.jpg'}
         alt='Hero Picture 4'
-        width={1920}
-        height={1440}
-        className='h-screen object-cover object-center'
+        width={7952}
+        height={5304}
+        className='relative h-screen object-cover object-center'
       />
     </Carousel>
   )
