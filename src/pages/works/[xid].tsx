cf124f41398f88c878ea2cb1074f7e6763689df6
akasz56 <akaasyahn@gmail.com>
2024-02-24 11:42:04 +0700

    fix: dynamic route redirect only if not found

--- src/pages/works/[xid].tsx
+++ src/pages/works/[xid].tsx
@@ -7,51 +7,56 @@ import { worksData, Work } from '../../components/constant'
 
 const WorkDetail: NextPage = () => {
   const router = useRouter()
-  const [workDetail, setWorkDetail] = useState<Work>()
+  const [data, setData] = useState<Work>()
 
   useEffect(() => {
-    if (!router.query.xid) router.push('/works')
-    if (typeof router.query.xid !== 'string') router.push('/works')
-
-    setWorkDetail(worksData.filter((item) => item.xid === router.query.xid)[0])
-    if (!worksData.filter((item) => item.xid === router.query.xid)[0]) router.push('/works')
+    if (router.query.xid && typeof router.query.xid === 'string') {
+      const workDetailData = worksData.filter((item) => item.xid === router.query.xid)[0]
+      if (!workDetailData) router.push('/works')
+      setData(workDetailData)
+    }
   }, [router])
 
-  return (
+  return data ? (
     <CustomHead
-      title={workDetail?.title ?? 'Detail Work'}
-      description={workDetail?.description}
+      title={data.title}
+      description={data.description}
     >
       <div className='asya-container'>
         <div className='flex'>
           <div className='w-1/2 space-y-12'>
-            {workDetail?.asset.map((item, key) =>
-              item.type === 'image' ? (
-                <Image
-                  key={key}
-                  src={item.src}
-                  alt={item.alt ?? ''}
-                  width={1080}
-                  height={1080}
-                  className='block object-cover'
-                />
-              ) : (
-                ''
-              )
-            )}
+            {data &&
+              data.asset
+                .sort((a) => Number(a.type === 'video'))
+                .map((item, key) =>
+                  item.type === 'image' ? (
+                    <Image
+                      key={key}
+                      src={item.src}
+                      alt={item.alt ?? data.title + key}
+                      width={1080}
+                      height={1080}
+                      className='block object-cover'
+                    />
+                  ) : (
+                    ''
+                  )
+                )}
           </div>
           <div className='w-1/2'>
             <div className='sticky top-0 ml-36 flex h-screen flex-col py-32'>
               <h3 className='flex-1 text-4xl font-bold uppercase leading-normal tracking-widest text-asya-dark'>
-                {workDetail?.title}
-                <span className='block text-xl font-normal tracking-widest'>{workDetail?.subtitle}</span>
+                {data.title}
+                <span className='block text-xl font-normal tracking-widest'>{data.subtitle}</span>
               </h3>
-              <p className='text-base leading-normal tracking-widest text-asya-dark'>{workDetail?.description}</p>
+              <p className='text-base leading-normal tracking-widest text-asya-dark'>{data.description}</p>
             </div>
           </div>
         </div>
       </div>
     </CustomHead>
+  ) : (
+    <></>
   )
 }
 
