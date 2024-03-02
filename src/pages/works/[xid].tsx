048e698e30fbb614c66ea5732d3ac634d600f74b
akasz56 <akaasyahn@gmail.com>
2024-03-02 15:31:39 +0700

    feat: mobile view

--- src/pages/works/[xid].tsx
+++ src/pages/works/[xid].tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import CustomHead from '@/layouts/CustomHead'
 import { useRouter } from 'next/router'
-import { worksData, Work } from '../../components/constant'
+import { worksData, Work } from '../../components/works'
 
 const WorkDetail: NextPage = () => {
   const router = useRouter()
@@ -22,49 +22,49 @@ const WorkDetail: NextPage = () => {
       title={data.title}
       description={data.description}
     >
-      <div className='asya-container'>
-        <div className='flex'>
-          <div className='w-1/2 space-y-12'>
-            {data &&
-              data.asset
-                .sort((a) => Number(a.type === 'video'))
-                .map((item, key) =>
-                  item.type === 'image' ? (
-                    <Image
-                      key={key}
+      <div className='flex flex-col lg:flex-row'>
+        <div className='space-y-12 lg:w-1/2'>
+          {data &&
+            data.asset
+              .sort((a) => Number(a.type === 'video'))
+              .map((item, key) =>
+                item.type === 'image' ? (
+                  <Image
+                    key={key}
+                    src={item.src}
+                    alt={item.alt ?? data.title + key}
+                    width={1080}
+                    height={1080}
+                    className='block object-cover'
+                  />
+                ) : item.type === 'video' ? (
+                  <video
+                    key={key}
+                    width={1920}
+                    height={1080}
+                    controls
+                    autoPlay={key === 0}
+                  >
+                    <source
                       src={item.src}
-                      alt={item.alt ?? data.title + key}
-                      width={1080}
-                      height={1080}
-                      className='block object-cover'
+                      type='video/mp4'
                     />
-                  ) : item.type === 'video' ? (
-                    <video
-                      key={key}
-                      width={1920}
-                      height={1080}
-                      controls
-                      autoPlay={key === 0}
-                    >
-                      <source
-                        src={item.src}
-                        type='video/mp4'
-                      />
-                      Your browser does not support the video tag.
-                    </video>
-                  ) : (
-                    ''
-                  )
-                )}
-          </div>
-          <div className='w-1/2'>
-            <div className='sticky top-0 ml-36 flex h-screen flex-col py-32'>
-              <h3 className='flex-1 text-4xl font-bold uppercase leading-normal tracking-widest text-asya-dark'>
-                {data.title}
-                <span className='block text-xl font-normal tracking-widest'>{data.subtitle}</span>
-              </h3>
-              <p className='text-base leading-normal tracking-widest text-asya-dark'>{data.description}</p>
-            </div>
+                    Your browser does not support the video tag.
+                  </video>
+                ) : (
+                  ''
+                )
+              )}
+        </div>
+        <div className='fixed bottom-0 h-1/2 bg-white lg:right-0 lg:h-screen lg:w-1/2'>
+          <div className='flex h-full flex-col px-6 py-8 lg:py-32 lg:pl-16 xl:pl-36'>
+            <h3 className='flex-1 text-4xl font-bold uppercase leading-normal tracking-widest text-asya-dark'>
+              {data.title}
+              <span className='block text-xl font-normal tracking-widest'>{data.subtitle}</span>
+            </h3>
+            <p className='text-base leading-normal tracking-widest text-asya-dark lg:max-w-[40em]'>
+              {data.description}
+            </p>
           </div>
         </div>
       </div>
