0eb8ee7d8344b90b40595c9f2b175e63caa14645
akasz56 <akaasyahn@gmail.com>
2024-05-15 23:21:08 +0700

    fix: overlapping hover img

--- src/components/WorksItem.tsx
+++ src/components/WorksItem.tsx
@@ -1,7 +1,7 @@
 import { Client } from '@/pages/works'
-import Image from 'next/image'
+import { useInView } from 'framer-motion'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface WorksItemProps {
   client: Client
@@ -10,6 +10,9 @@ interface WorksItemProps {
 export default function WorksItem(props: WorksItemProps) {
   const [isHovered, setIsHovered] = useState(false)
   const [display, setDisplay] = useState(false)
+
+  const ref = useRef(null)
+  const isInFrame = useInView(ref)
 
   useEffect(() => {
     if (!display && isHovered) {
@@ -23,7 +26,10 @@ export default function WorksItem(props: WorksItemProps) {
   }, [isHovered, display])
 
   return (
-    <li className='mb-12'>
+    <li
+      className='mb-12'
+      ref={ref}
+    >
       <Link
         href={'/works/project-one'}
         className='flex'
@@ -34,15 +40,17 @@ export default function WorksItem(props: WorksItemProps) {
         <hr className='flex-1 self-center border-asya-dark' />
         <span className='ml-20 w-14 text-xl leading-relaxed tracking-widest text-asya-dark'>{props.client.year}</span>
       </Link>
-      <div
-        className='hover-img'
-        style={{ opacity: display ? 1 : 0 }}
-      >
-        <img
-          src={props.client.image}
-          alt={props.client.name}
-        />
-      </div>
+      {isInFrame && (
+        <div
+          className='hover-img'
+          style={{ opacity: display ? 1 : 0 }}
+        >
+          <img
+            src={props.client.image}
+            alt={props.client.name}
+          />
+        </div>
+      )}
     </li>
   )
 }
