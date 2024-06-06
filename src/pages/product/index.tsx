d047ca2f0f8fbf663a90fbeab176100e7ef4f08c
akasz56 <akaasyahn@gmail.com>
2024-06-07 01:42:09 +0700

    feat: product page

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,120 +1,147 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NextPage } from 'next'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
 import CustomHead from '@/layouts/CustomHead'
-import test from '../../../public/images/hero1.jpg'
+import { productImages } from '@/components/products'
+import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
 
 const Index: NextPage = () => {
-  const collections = {
-    title: 'All Collection',
-    collections: ['Collection A', 'Lorem Ipsum', 'Collection B', 'Lorem Ipsum', 'Collection C', 'Lorem Ipsum'],
-  }
+  let duration = 5500
+  const [value, setValue] = useState(0)
 
-  const availability = {
-    title: 'Availability',
-    collections: ['In Stock (10)', 'Out of Stock (3)'],
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prevValue) => (prevValue < productImages.length - 1 ? prevValue + 1 : 0))
+    }, duration)
 
-  const section = {
-    visible: { transition: { staggerChildren: 0.5 } },
-  }
-
-  const item = {
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.5 },
-    },
-    hidden: { opacity: 0, y: 25 },
-  }
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <CustomHead
       title='Product'
       description='Product Page'
     >
-      <div className='relative h-[50vh] overflow-hidden'>
-        <Image
-          src={test}
-          alt={'image'}
-          className='absolute -bottom-64 -z-10 object-cover'
-        />
-        <motion.div
-          id='hero'
-          initial='hidden'
-          variants={section}
-          whileInView='visible'
-          className='flex h-full items-center justify-center'
-        >
-          <motion.p
-            variants={item}
-            className='text-center text-4xl font-medium leading-normal tracking-widest text-white 2xl:text-5xl 2xl:leading-normal'
+      <AnimatePresence>
+        <div className='relative overflow-hidden lg:h-screen'>
+          <div className='flex w-full overflow-hidden lg:absolute lg:h-screen'>
+            <Image
+              width={1920}
+              height={1800}
+              alt='image gallery'
+              src={productImages[value == 0 ? productImages.length - 1 : value - 1][0]}
+              className='w-full object-cover lg:min-h-screen lg:w-1/2 lg:object-right'
+            />
+            <Image
+              width={1920}
+              height={1800}
+              alt='image gallery'
+              src={productImages[value == 0 ? productImages.length - 1 : value - 1][1]}
+              className='hidden object-cover lg:block lg:min-h-screen lg:w-1/2 lg:object-left'
+            />
+          </div>
+          <motion.div
+            key={value}
+            initial={{ y: '150%' }}
+            animate={{ y: 0, transition: { duration: 5, ease: 'easeInOut' } }}
+            className='absolute top-0 flex w-full overflow-hidden lg:h-screen'
           >
-            Our Newest Collection “Lorem Ipsum”
-          </motion.p>
-        </motion.div>
-      </div>
-
-      <section
-        id='catalog'
-        className='asya-container flex py-20 xl:py-40'
-      >
-        <div className='flex w-1/4 flex-col items-center space-y-8'>
-          <ul>
-            <li className='mb-8 text-xl font-bold leading-relaxed tracking-widest text-asya-dark'>
-              {collections.title}
-            </li>
-            {collections.collections.map((item, key) => (
-              <li key={key}>
-                <a
-                  href='#'
-                  className='asya-link text-xl leading-relaxed tracking-widest text-asya-dark'
-                >
-                  <span className='after:bg-asya-dark'>{item}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
-          <ul>
-            <li className='mb-8 text-xl font-bold leading-relaxed tracking-widest text-asya-dark'>
-              {availability.title}
-            </li>
-            {availability.collections.map((item, key) => (
-              <li key={key}>
-                <a
-                  href='#'
-                  className='asya-link text-xl leading-relaxed tracking-widest text-asya-dark'
-                >
-                  <span className='after:bg-asya-dark'>{item}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
+            <Image
+              width={1920}
+              height={1800}
+              alt='image gallery'
+              src={productImages[value][0]}
+              className='w-full object-cover lg:min-h-screen lg:w-1/2 lg:object-right'
+            />
+            <Image
+              width={1920}
+              height={1800}
+              alt='image gallery'
+              src={productImages[value][1]}
+              className='hidden object-cover lg:block lg:min-h-screen lg:w-1/2 lg:object-left'
+            />
+          </motion.div>
         </div>
-        <ul className='grid flex-1 grid-cols-2 gap-4 lg:grid-cols-3 xl:gap-8'>
-          {[...Array(12)].map((item, key) => (
-            <motion.li
-              key={key}
-              className='m-8'
-              whileHover={{ scale: 1.05 }}
-            >
-              <Link href={'/product/product-one'}>
-                <div className='aspect-square bg-asya-dark'></div>
-                <div className='mt-6 block'>
-                  <p className='text-center text-xl font-bold leading-relaxed tracking-widest text-asya-dark'>
-                    Hijab Lorem Ipsum
-                  </p>
-                  <p className='text-center text-xl leading-relaxed tracking-widest text-asya-dark'>Rp200.000</p>
-                </div>
-              </Link>
-            </motion.li>
-          ))}
-        </ul>
-      </section>
+      </AnimatePresence>
     </CustomHead>
   )
 }
 
 export default Index
+
+// Just in case you dont want to use framer-motion
+const easeInOutQuad = (t: number) => {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+}
+const IndexManual: NextPage = () => {
+  const [imgIdx, setImgIdx] = useState(0)
+  const [pos, setPos] = useState(100)
+  const [startTime, setStartTime] = useState(0)
+  const duration = 2000
+  const delay = 2000
+
+  function preloadImages() {
+    productImages.forEach((pair) => {
+      pair.forEach((link) => {
+        let img = new Image()
+        img.src = link
+      })
+    })
+  }
+  preloadImages()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = Date.now()
+      if (startTime === 0) {
+        setStartTime(now)
+      }
+      const elapsedTime = now - startTime
+
+      if (elapsedTime >= duration) {
+        if (elapsedTime >= duration + delay) {
+          setImgIdx((prevValue) => (prevValue < productImages.length - 1 ? prevValue + 1 : 0))
+          setStartTime(now)
+        }
+      } else {
+        const easingValue = easeInOutQuad(elapsedTime / duration)
+        setPos(100 - easingValue * 100)
+      }
+    }, 10)
+
+    return () => clearInterval(interval)
+  }, [startTime])
+
+  return (
+    <CustomHead
+      title='Product'
+      description='Product Page'
+    >
+      <div className='relative h-screen overflow-hidden'>
+        <div className='absolute flex h-screen w-full overflow-hidden'>
+          <img
+            src={productImages[imgIdx == 0 ? productImages.length - 1 : imgIdx - 1][0]}
+            className='w-full object-cover lg:min-h-screen lg:w-1/2 lg:object-right'
+          />
+          <img
+            src={productImages[imgIdx == 0 ? productImages.length - 1 : imgIdx - 1][1]}
+            className='hidden object-cover lg:block lg:min-h-screen lg:w-1/2 lg:object-left'
+          />
+        </div>
+        <div
+          style={{ transform: `translateY(${pos}%)` }}
+          className='absolute flex h-screen w-full overflow-hidden'
+        >
+          <img
+            src={productImages[imgIdx][0]}
+            className='w-full object-cover lg:min-h-screen lg:w-1/2 lg:object-right'
+          />
+          <img
+            src={productImages[imgIdx][1]}
+            className='hidden object-cover lg:block lg:min-h-screen lg:w-1/2 lg:object-left'
+          />
+        </div>
+      </div>
+    </CustomHead>
+  )
+}
